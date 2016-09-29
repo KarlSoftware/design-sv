63278df029ac63b308c97de7dd590aac520c9492
sbtn <sbtn@users.noreply.github.com>
2016-09-29 11:21:42 +0200

    Added chart

--- js/index.js
+++ js/index.js
@@ -18,16 +18,16 @@ var geoJson;
 // Panel - custom control
 var info = L.control();
 
-info.onAdd = function (map) {
+info.onAdd = function(map) {
     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
     this.update();
     return this._div;
 };
 
 // method that we will use to update the control based on feature properties passed
-info.update = function (props) {
+info.update = function(props) {
     var now = new Date();
-    this._div.innerHTML = (props ? '<h4>' + props.knnamn + '</h4><br /><p>' + now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay() + '</p><p><strong><em>Platsannonser: ' + props.platser + '</em></strong></p>': '');
+    this._div.innerHTML = (props ? '<h4>' + props.knnamn + '</h4><br /><p>' + now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay() + '</p><p><strong><em>Platsannonser: ' + props.platser + '</em></strong></p>' : '');
 };
 
 info.addTo(map);
@@ -72,15 +72,15 @@ function onEachFeature(feature, layer) {
 
 function style(feature) {
     return {
-        fillOpacity: 0, 
-        color: "#7eeaee", 
+        fillOpacity: 0,
+        color: "#7eeaee",
         weight: feature.properties.platser > 0 ? 0.1 : 0,
     };
 }
 
 var heat = L.heatLayer([], {
-    radius: 30, 
-    maxZoom: 5, 
+    radius: 30,
+    maxZoom: 5,
     gradient: { .4: "#0085da", .6: "#0088a5", .7: "#45b6ba", .8: "#7eeaee", 1: "white" }
 }).addTo(map);
 
@@ -107,33 +107,111 @@ $.getJSON('./json_AF/' + today() + '_json_AF.json', function(jobs) {
                 if (match.kommunnamn == point.properties.knnamn) {
                     point.properties.platser == null ? point.properties.platser = 1 : point.properties.platser += 1;
                     kn_lan_poly.features[i].properties.platser == null ? kn_lan_poly.features[i].properties.platser = 1 : kn_lan_poly.features[i].properties.platser += 1;
-                    
+
                 }
-                kn_lan_poly.features[i].properties.platser == null ? kn_lan_poly.features[i].properties.platser = 0 : null ;
+                kn_lan_poly.features[i].properties.platser == null ? kn_lan_poly.features[i].properties.platser = 0 : null;
             });
             point.properties.platser > highest ? highest = point.properties.platser : highest = highest;
 
-            
+
         });
-        
-})
-.done(function(jobs) {
-    addPoints();
-    console.log("Success!");
-})
-.fail(function(jqxhr, textStatus, error) {
-    var err = textStatus + ", " + error;
-    console.log("Request Failed: " + err);
-});
+
+    })
+    .done(function(jobs) {
+        addPoints();
+        console.log("Success!");
+    })
+    .fail(function(jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+        console.log("Request Failed: " + err);
+    });
 
 
 function addPoints() {
-    $.each(kn_points.features, function (i, point) {
+    $.each(kn_points.features, function(i, point) {
         if (point.properties.platser > 0) {
             point.properties.density = 0.0 + (1.0 - 0.0) * (point.properties.platser - 0) / (highest - 0);
             heat.addLatLng([point.geometry.coordinates[1], point.geometry.coordinates[0], point.properties.density]);
         }
     });
 
-    geoJson = L.geoJson(kn_lan_poly, {style: style, onEachFeature: onEachFeature }).addTo(map);
+    geoJson = L.geoJson(kn_lan_poly, { style: style, onEachFeature: onEachFeature }).addTo(map);
+
+    var ranking = [];
+
+    $.each(kn_lan_poly.features, function(i, feature) {
+        ranking.push(feature.properties);
+    });
+
+    ranking.sort(function(a, b) {
+        return a.platser - b.platser;
+    });
+
+    var top5 = [];
+
+    for (var i = ranking.length - 5; i < ranking.length; i++) {
+        top5.push(ranking[i]);
+    }
+
+
+
+    // Chart - custom control
+    var chart = L.control({
+        position: 'bottomright'
+    });
+
+    chart.onAdd = function(map) {
+        this._div = L.DomUtil.create('div', 'char'); // create a div with a class "chart"
+        this._div.innerHTML = '<div id="gchart"></div>';
+        return this._div;
+    };
+
+    chart.addTo(map);
+
+    var data = [{
+        x: [top5[0].knnamn, top5[1].knnamn, top5[2].knnamn, top5[3].knnamn, top5[4].knnamn],
+        y: [top5[0].platser, top5[1].platser, top5[2].platser, top5[3].platser, top5[4].platser]
+    }];
+
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', { 'packages': ['corechart'] });
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Kommun');
+        data.addColumn('number', 'Platser');
+        data.addRows( top5.map(function (obj) {
+            return [obj.knnamn, obj.platser];
+        }));
+
+        // Set chart options
+        var options = {
+            'title': "",
+            'titleTextStyle': {'color': 'white', 'fontSize': 40},
+            'width': 400,
+            'height': 300,
+            'backgroundColor': 'transparent',
+            'colors': ['white','white','white','white','white'],
+            'legend': {'position': 'none'},
+            'animation': { 'startup': true, 'duration': 1000},
+            'axisTitlesPosition': 'none',
+            'hAxis': {'textPosition': 'none', 'gridlines': {'color': 'transparent', 'count': -1}},
+            'vAxis': {'textPosition': 'none', 'textStyle': {'color': "white"}}
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var gchart = new google.visualization.BarChart(document.getElementById('gchart'));
+        gchart.draw(data, options);
+    }
+
+
+    console.log(top5);
 }
